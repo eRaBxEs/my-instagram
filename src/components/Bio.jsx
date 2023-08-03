4b49e1834afbe0a29e151b40b9036de6a96bce89
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-08-03 23:55:41 +0100

    add index storage for bio

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import getPhotoUrl from 'get-photo-url'
 import profileIcon from '../assets/profileIcon.svg'
+import { db } from '../dexie'
 
 const Bio = () => {
     // destructuring
@@ -9,16 +11,35 @@ const Bio = () => {
     }) // the string values refers to the use state hook' 
 
     const [editFormIsOpen, setEditFormIsOpen] = useState(false)
+    const [profilePhoto, setProfilePhoto] = useState(profileIcon)
 
+    // useEffect will be the first function to run when our components loads up
+    useEffect(() => {
+        const setDataFromDb = async () => {
+            const userDetailsFromDb = await db.bio.get('info')
+            const profilePhotoFromDb = await db.bio.get('profilePhoto')
+            userDetailsFromDb && setUserDetails(userDetailsFromDb)
+            profilePhotoFromDb && setProfilePhoto(profilePhotoFromDb)
+        }
 
-    const updateUserDetails = (event) => {
+        setDataFromDb()
+    })
+
+    const updateUserDetails = async (event) => {
         event.preventDefault()
-        setUserDetails({
+        const objectData = {
             name: event.target.nameOfUser.value,
             about: event.target.aboutUser.value,
-        })
-
+        }
+        setUserDetails(objectData)
+        await db.bio.put(objectData, 'info')
         setEditFormIsOpen(false) // after successfully updating the user details, set edit form as false
+    }
+
+    const updateProfilePhoto = async () => {
+        const newProfilePhoto = await getPhotoUrl('#profilePhotoInput')
+        setProfilePhoto(newProfilePhoto)
+        await db.bio.put(newProfilePhoto, 'profilePhoto')
     }
 
     const editForm = (
@@ -32,12 +53,17 @@ const Bio = () => {
     )
 
     const editButton = <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
+    
 
     return (
         <section className="bio">
-            <div className="profile-photo" role="button" title="Click to edit photo">
-                <img src={profileIcon} alt="" />
-            </div>
+            <input type="file" accept='image/*' name='photo' id='profilePhotoInput' />
+            <label htmlFor="profilePhotoInput" onClick={updateProfilePhoto}>
+                <div className="profile-photo" role="button" title="Click to edit photo">
+                    <img src={profilePhoto} alt="profile" />
+                </div> 
+            </label>
+           
             <div className="profile-info">
                 <p className="name">{userDetails.name}</p>
                 <p className="about">{userDetails.about}</p>
