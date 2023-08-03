cd6406f77eeebc588cd4c261c7506c46b1c6d87e
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-08-03 23:56:52 +0100

    add reactive feature to add image

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,38 +1,35 @@
-import pexels1 from '../assets/pexels1.jpeg'
-import pexels2 from '../assets/pexels2.jpeg'
-import pexels3 from '../assets/pexels3.jpeg'
-import pexels4 from '../assets/pexels4.jpeg'
-import pexels5 from '../assets/pexels5.jpeg'
+import { useState } from 'react'
+import getPhotoUrl from 'get-photo-url'
+
 
 const Gallery = () => {
+
+    const [allPhotos, setAllPhotos] = useState([])
+
+    const addPhoto = async () => {
+        const newPhoto = {
+            id : Date.now(),
+            url: await getPhotoUrl('#addPhotoInput'),
+        }
+
+        setAllPhotos([newPhoto, ...allPhotos])  // spreading
+    }
+
+
     return (
         <>
             <input type="file" name="photo" id="addPhotoInput" />
-            <label htmlFor="addPhotoInput">
+            <label htmlFor="addPhotoInput" onClick={addPhoto}>
                 <i className="add-photo-button fas fa-plus-square"></i>
             </label>
 
             <section className='gallery'>
-                <div className="item">
-                    <img src={pexels1} className="item-image" alt="" />
-                    <button className="delete-button">Delete</button>
-                </div>
-                <div className="item">
-                    <img src={pexels2} className="item-image" alt="" />
-                    <button className="delete-button">Delete</button>
-                </div>
-                <div className="item">
-                    <img src={pexels3} className="item-image" alt="" />
-                    <button className="delete-button">Delete</button>
-                </div>
-                <div className="item">
-                    <img src={pexels4} className="item-image" alt="" />
-                    <button className="delete-button">Delete</button>
-                </div>
-                <div className="item">
-                    <img src={pexels5} className="item-image" alt="" />
-                    <button className="delete-button">Delete</button>
-                </div>
+                {allPhotos.map((photo) => (
+                    <div className="item" key={photo.id}>
+                        <img src={photo.url} className="item-image" alt="" />
+                        <button className="delete-button">Delete</button>
+                    </div>
+                ))}
 
             </section>
         </>
