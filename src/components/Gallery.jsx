e0febfa7b3e04775e41f646b3c27c4ff29a128fa
Ehioje Henry Erabor <ehiojee@yahoo.com>
2023-08-04 01:05:00 +0100

    use live query from dexie react hooks

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,21 @@
-import { useState } from 'react'
 import getPhotoUrl from 'get-photo-url'
+import { useLiveQuery } from 'dexie-react-hooks'
+import { db } from '../dexie'
 
 
 const Gallery = () => {
 
-    const [allPhotos, setAllPhotos] = useState([])
+    const allPhotos = useLiveQuery(() => db.gallery.toArray(), [])
 
     const addPhoto = async () => {
-        const newPhoto = {
-            id : Date.now(),
+        db.gallery.add({
             url: await getPhotoUrl('#addPhotoInput'),
-        }
-
-        setAllPhotos([newPhoto, ...allPhotos])  // spreading
+        })
     }
 
+    const removePhoto = (id) => {
+        db.gallery.delete(id)
+    }
 
     return (
         <>
@@ -24,10 +25,11 @@ const Gallery = () => {
             </label>
 
             <section className='gallery'>
-                {allPhotos.map((photo) => (
+                {!allPhotos && <p>Loading...</p>}
+                {allPhotos?.map((photo) => (
                     <div className="item" key={photo.id}>
                         <img src={photo.url} className="item-image" alt="" />
-                        <button className="delete-button">Delete</button>
+                        <button className="delete-button" onClick={() => removePhoto(photo.id)}>Delete</button>
                     </div>
                 ))}
 
